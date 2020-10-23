0d28640d05928184b5be465cd3483f42d1428ae8
SergioVelazquezSerrano <sergio@bcninfoservices.com>
2020-10-23 14:05:08 +0200

    three

--- THREE/geo.js
+++ THREE/geo.js
@@ -8,8 +8,15 @@ function load(){
     var mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100);
 
 
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(new THREE.Color(0xFF0000, 1.0));
+    document.body.appendChild( renderer.domElement );
+
+    render.render(escena, camara);
 }
 
 
